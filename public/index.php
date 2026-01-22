6f09c3c033b4665b82bf60dfdd137ef9500ef13e
oldman <oldm2563@email.com>
2026-01-22 15:08:25 +0100

    added some routes

--- public/index.php
+++ public/index.php
@@ -16,6 +16,12 @@ $routes = [
         "GET /student" => [StudentController::class , "studentboard"],
         "GET /teacher" => [TeacherController::class , "teacherboard"],
         "GET /teacher/addstudent" => [TeacherController::class , "add_student_form"],
+        "POST /teacher/addstudent" => [TeacherController::class , "add_student"],
+        "GET /teacher/addclass" => [TeacherController::class , "add_class_form"],
+        "POST /teacher/addclass" => [TeacherController::class , "add_class"],
+        "GET /teacher/assign" => [TeacherController::class , "assign_class_form"],
+        "POST /teacher/assign" => [TeacherController::class , "assign_class"],
+        "POST /teacher/work" => [TeacherController::class , "add_work"],
     ];
 
 $router->route($routes);
